7f2eb8796875c593f31d9a89d235c3221103c927
ethereal-sheep <55541825+ethereal-sheep@users.noreply.github.com>
2022-08-11 01:14:24 +0800

    predicate support
    added *_with functions for predicate support
    added PredHeapIterator to iterate over a predicated made heap

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,11 @@
 //! ```
 
 #![warn(rust_2018_idioms)]
+use core::cmp::Ordering;
 
 /// Transforms the given slice into a maximal heap.
 ///
-/// After `make_heap` is called the maximal element of the slice is located
+/// After `make_heap` is called, the maximal element of the slice is located
 /// at `slice[0]`.
 ///
 /// # Examples
@@ -33,9 +34,27 @@
 /// assert_eq!(peek_heap(&mut arr), Some(&9));
 /// ```
 pub fn make_heap<T: PartialOrd>(slice: &mut [T]) {
+    make_heap_with(slice, T::partial_cmp)
+}
+
+/// Transforms the given slice into a heap with a given predicate.
+///
+/// After `make_heap_with` is called, the top element of the heap is located
+/// at `slice[0]`.
+///
+/// # Examples
+///
+/// ```
+/// use heapify::*;
+/// let mut arr = [5, 7, 9];
+/// make_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(arr[0], 5);
+/// assert_eq!(peek_heap(&mut arr), Some(&5));
+/// ```
+pub fn make_heap_with<T>(slice: &mut [T], pred: fn(&T, &T) -> Option<Ordering>) {
     let n = slice.len();
     for i in (0..=((n - 1) / 2)).rev() {
-        bubble_down(slice, i);
+        bubble_down(slice, i, pred);
     }
 }
 
@@ -52,6 +71,24 @@ pub fn make_heap<T: PartialOrd>(slice: &mut [T]) {
 pub fn make_heap_iter<T: PartialOrd>(slice: &mut [T]) -> HeapIterator<'_, T> {
     make_heap(slice);
     HeapIterator { heap: slice }
+}
+
+/// Creates a `PredHeapIterator<T>` from a given slice and a given predicate.
+///
+/// # Examples
+///
+/// ```
+/// use heapify::*;
+/// let mut arr = [5, 7, 9];
+/// let mut iter = make_heap_iter_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(iter.next().cloned(), Some(5));
+/// ```
+pub fn make_heap_iter_with<T>(
+    slice: &mut [T],
+    pred: fn(&T, &T) -> Option<Ordering>,
+) -> PredHeapIterator<'_, T> {
+    make_heap_with(slice, pred);
+    PredHeapIterator { heap: slice, pred }
 }
 
 /// Pushes an element into a heap.
@@ -77,7 +114,33 @@ pub fn make_heap_iter<T: PartialOrd>(slice: &mut [T]) -> HeapIterator<'_, T> {
 /// assert_eq!(peek_heap(&mut vec), Some(&10));
 /// ```
 pub fn push_heap<T: PartialOrd>(slice: &mut [T]) {
-    bubble_up(slice);
+    bubble_up(slice, T::partial_cmp);
+}
+
+/// Pushes an element into a heap, with a given predicate.
+///
+/// Given a slice where `slice[0..len-1]` is a heap, then the element at
+/// `slice[len-1]` is pushed into the heap by bubbling it up to its correct
+/// position.
+///
+/// # Examples
+///
+/// ```
+/// use heapify::*;
+/// let mut vec = vec![5, 7, 9];
+/// make_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(peek_heap(&mut vec), Some(&5));
+///
+/// vec.push(8);
+/// push_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(peek_heap(&mut vec), Some(&5));
+///
+/// vec.push(3);
+/// push_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(peek_heap(&mut vec), Some(&3));
+/// ```
+pub fn push_heap_with<T>(slice: &mut [T], pred: fn(&T, &T) -> Option<Ordering>) {
+    bubble_up(slice, pred);
 }
 
 /// Pops an element from a maximal heap.
@@ -106,7 +169,36 @@ pub fn pop_heap<T: PartialOrd>(slice: &mut [T]) {
     let n = slice.len();
     if n > 1 {
         slice.swap(0, n - 1);
-        bubble_down(&mut slice[0..n - 1], 0);
+        bubble_down(&mut slice[0..n - 1], 0, T::partial_cmp);
+    }
+}
+/// Pops an element from a maximal heap, with a given predicate.
+///
+/// Given a heap, the maximal element at `slice[0]` is moved to `slice[len-1]`
+/// and the heap invariant is maintained for `slice[0..len-1]`.
+///
+/// # Examples
+///
+/// ```
+/// use heapify::*;
+/// let mut vec = vec![5, 7, 9];
+/// make_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(peek_heap(&mut vec), Some(&9));
+///
+/// pop_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(vec.pop(), Some(5));
+///
+/// pop_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(vec.pop(), Some(7));
+///
+/// pop_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// assert_eq!(vec.pop(), Some(9));
+/// ```
+pub fn pop_heap_with<T>(slice: &mut [T], pred: fn(&T, &T) -> Option<Ordering>) {
+    let n = slice.len();
+    if n > 1 {
+        slice.swap(0, n - 1);
+        bubble_down(&mut slice[0..n - 1], 0, pred);
     }
 }
 
@@ -122,7 +214,7 @@ pub fn pop_heap<T: PartialOrd>(slice: &mut [T]) {
 /// let mut empty = [0; 0];
 /// assert_eq!(peek_heap(&mut empty), None);
 /// ```
-pub fn peek_heap<T: PartialOrd>(slice: &mut [T]) -> Option<&T> {
+pub fn peek_heap<T>(slice: &mut [T]) -> Option<&T> {
     let n = slice.len();
     if n > 0 {
         Some(&slice[0])
@@ -131,34 +223,38 @@ pub fn peek_heap<T: PartialOrd>(slice: &mut [T]) -> Option<&T> {
     }
 }
 
-fn bubble_up<T: PartialOrd>(slice: &mut [T]) {
+fn pred_greater<T>(pred: fn(&T, &T) -> Option<Ordering>, lhs: &T, rhs: &T) -> bool {
+    pred(lhs, rhs) == Some(Ordering::Greater)
+}
+
+fn bubble_up<T>(slice: &mut [T], pred: fn(&T, &T) -> Option<Ordering>) {
     let n = slice.len();
     let mut i = n;
     let mut p = i / 2;
-    while p > 0 && (slice[p - 1] < slice[i - 1]) {
+    while p > 0 && (pred_greater(pred, &slice[i - 1], &slice[p - 1])) {
         slice.swap(p - 1, i - 1);
         i = p;
         p = i / 2;
     }
 }
 
-fn bubble_down<T: PartialOrd>(slice: &mut [T], index: usize) {
+fn bubble_down<T>(slice: &mut [T], index: usize, pred: fn(&T, &T) -> Option<Ordering>) {
     let n = slice.len();
     let mut i = index;
     let mut l = i * 2 + 1;
     let mut r = i * 2 + 2;
 
-    if r < n && slice[r] > slice[l] {
+    if r < n && pred_greater(pred, &slice[r], &slice[l]) {
         l = r;
     }
     // invariant: slice[l] >= slice[r]
     // if slice[l] > slice[i], do push
-    while l < n && slice[l] > slice[i] {
+    while l < n && pred_greater(pred, &slice[l], &slice[i]) {
         slice.swap(i, l);
         i = l;
         l = i * 2 + 1;
         r = i * 2 + 2;
-        if r < n && slice[r] > slice[l] {
+        if r < n && pred_greater(pred, &slice[r], &slice[l]) {
             l = r;
         }
     }
@@ -185,7 +281,7 @@ fn bubble_down<T: PartialOrd>(slice: &mut [T], index: usize) {
 /// ```
 #[derive(Debug)]
 pub struct HeapIterator<'a, T: PartialOrd> {
-    heap: &'a mut [T],
+    heap: &'a mut [T]
 }
 
 impl<'a, T: PartialOrd> core::iter::Iterator for HeapIterator<'a, T> {
@@ -195,6 +291,58 @@ impl<'a, T: PartialOrd> core::iter::Iterator for HeapIterator<'a, T> {
         let n = self.heap.len();
         if n > 0 {
             pop_heap(self.heap);
+
+            unsafe {
+                let old = core::ptr::read(&self.heap);
+                let (left, right) = old.split_at_mut(n - 1);
+                core::ptr::write(&mut self.heap, left);
+                assert_eq!(right.len(), 1);
+
+                Some(&mut right[0])
+            }
+        } else {
+            None
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.heap.len(), Some(self.heap.len()))
+    }
+}
+
+/// An iterator type to iterate upon a heap with a given predicate.
+/// 
+/// Larger than HeapIterator due to storing of the function pointer predicate.
+///
+/// A complete iteration has the side effect of sorting the underlying
+/// slice in order provided by predicate.
+///
+/// # Examples
+/// Basic usage:
+/// ```
+/// use heapify::*;
+/// let mut arr = [5, 7, 9];
+/// let iter = make_heap_iter_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+/// for item in iter {
+///     print!("{} ", item);
+/// }
+/// ```
+/// This will print:
+/// ``` text
+/// 5 7 9
+/// ```
+pub struct PredHeapIterator<'a, T> {
+    heap: &'a mut [T],
+    pred: fn(&T, &T) -> Option<Ordering>
+}
+
+impl<'a, T> core::iter::Iterator for PredHeapIterator<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let n = self.heap.len();
+        if n > 0 {
+            pop_heap_with(self.heap, self.pred);
 
             unsafe {
                 let old = core::ptr::read(&self.heap);
@@ -236,6 +384,14 @@ mod tests {
     }
 
     #[test]
+    fn test_make_heap_with() {
+        let mut arr = [5, 7, 9];
+        make_heap_with(&mut arr, |lhs, rhs| rhs.partial_cmp(lhs));
+        assert_eq!(arr[0], 5);
+        assert_eq!(peek_heap(&mut arr), Some(&5));
+    }
+
+    #[test]
     fn test_push_heap() {
         let mut vec = vec![5, 7, 9];
         make_heap(&mut vec);
@@ -248,6 +404,22 @@ mod tests {
         vec.push(10);
         push_heap(&mut vec);
         assert_eq!(peek_heap(&mut vec), Some(&10));
+    }
+
+    #[test]
+    fn test_push_heap_with() {
+        let mut vec = vec![5, 7, 9];
+        let pred: fn(&i32, &i32) -> Option<Ordering> = |lhs, rhs| rhs.partial_cmp(lhs);
+        make_heap_with(&mut vec, pred);
+        assert_eq!(peek_heap(&mut vec), Some(&5));
+
+        vec.push(8);
+        push_heap_with(&mut vec, pred);
+        assert_eq!(peek_heap(&mut vec), Some(&5));
+
+        vec.push(3);
+        push_heap_with(&mut vec, pred);
+        assert_eq!(peek_heap(&mut vec), Some(&3));
     }
 
     #[test]
@@ -264,6 +436,23 @@ mod tests {
 
         pop_heap(&mut vec);
         assert_eq!(vec.pop(), Some(5));
+    }
+    
+    #[test]
+    fn test_pop_heap_with() {
+        let mut vec = vec![5, 7, 9];
+        let pred: fn(&i32, &i32) -> Option<Ordering> = |lhs, rhs| rhs.partial_cmp(lhs);
+        make_heap_with(&mut vec, pred);
+        assert_eq!(peek_heap(&mut vec), Some(&5));
+
+        pop_heap_with(&mut vec, pred);
+        assert_eq!(vec.pop(), Some(5));
+
+        pop_heap_with(&mut vec, pred);
+        assert_eq!(vec.pop(), Some(7));
+
+        pop_heap_with(&mut vec, pred);
+        assert_eq!(vec.pop(), Some(9));
     }
 
     #[test]
@@ -295,5 +484,28 @@ mod tests {
         });
 
         assert_eq!(vec, vec![1, 0, 3, 0, 5, 0]);
+    }
+
+    #[test]
+    fn test_pred_heap_iterator_sorted() {
+        let pred: fn(&i32, &i32) -> Option<Ordering> = |lhs, rhs| rhs.partial_cmp(lhs);
+        let mut vec = vec![1, 9, 2, 8, 3, 7];
+        let iter = make_heap_iter_with(&mut vec, pred);
+        for _ in iter {}
+        assert_eq!(vec, vec![9, 8, 7, 3, 2, 1]);
+    }
+
+    #[test]
+    fn test_pred_heap_iterator_mut() {
+        let pred: fn(&i32, &i32) -> Option<Ordering> = |lhs, rhs| rhs.partial_cmp(lhs);
+        let mut vec = vec![1, 2, 3, 4, 5, 6];
+        let iter = make_heap_iter_with(&mut vec, pred);
+        iter.for_each(|i| {
+            if *i % 2 == 1 {
+                *i = 0
+            }
+        });
+
+        assert_eq!(vec, vec![6, 0, 4, 0, 2, 0]);
     }
 }
